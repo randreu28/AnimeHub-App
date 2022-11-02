36c1b11eb8c39154ec57b4fe8dc04336ac39b2e6
randreu28 <randreu28@gmail.com>
2022-11-02 13:03:11 +0100

    Removes boilerplate and creates a product page

---
+++ lib/product_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("Hello world"),
+    ));
+  }
+}
