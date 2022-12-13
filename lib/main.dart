8530fc692337f52c2ef6f9b23d3fe80f106fc8a7
Alex Coronado <alex.coronado@estudiantat.upc.edu>
2022-12-13 16:18:54 +0100

    Conecto a Firebase
    - A falta de Cloud Firestore.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:p1_coronado/anime_screen.dart';
+import 'package:p1_coronado/firebase_options.dart';
 import 'package:p1_coronado/model/anime.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
