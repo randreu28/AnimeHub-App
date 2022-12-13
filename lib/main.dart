a904b24da8589883f918d152f49936651f6b273b
Alex Coronado <alex.coronado@estudiantat.upc.edu>
2022-12-13 16:31:11 +0100

    he hecho un cambio
    he hecho un cambio espectacular en el main

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return Provider.value(
