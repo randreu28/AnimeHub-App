81606dc658d22eda54ba2c59e26d4a9e8d7a5090
randreu28 <randreu28@gmail.com>
2022-12-13 21:30:45 +0100

    Corrige la importación de las credencailes firebase
    ESto daba error en CI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:p1_coronado/anime_screen.dart';
-import 'package:p1_coronado/firebase_options.dart';
 import 'package:p1_coronado/model/anime.dart';
 import 'package:provider/provider.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +17,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return Provider.value(
