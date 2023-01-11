ccbd2164010ec2ee61da354fc6c9e5b90ea7e85a
Alex Coronado <alex.coronado@estudiantat.upc.edu>
2023-01-11 13:09:17 +0100

    prueba de imagen

--- lib/screens/my_list_screen.dart
+++ lib/screens/my_list_screen.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class MyListScreen extends StatelessWidget {
+class MyListScreen extends StatefulWidget {
   const MyListScreen({super.key});
 
   @override
+  State<MyListScreen> createState() => _MyListScreenState();
+}
+
+class _MyListScreenState extends State<MyListScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Discover screen"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Column(
+            children: const [Text("MYLIST")],
+          ),
+          Row(
+            children: [
+              Image.network(
+                  "https://cdn.myanimelist.net/images/anime/1471/128323.jpg",
+                  height: 250),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
