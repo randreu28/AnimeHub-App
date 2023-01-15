6ce6a1d0e71565d3d15a3f7cde6d3ebc002466f2
Rubén Chiquin <randreu28@gmail.com>
2023-01-15 14:59:53 +0100

    Crea un prototipo de barra de búsqueda

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,12 +1,50 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
 
   @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  String query = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Discover screen"),
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  query = value;
+                });
+              },
+              onSubmitted: (value) {
+                /* TODO: Search anime and display it */
+              },
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none),
+                hintText: 'Search',
+                hintStyle: const TextStyle(color: Colors.grey, fontSize: 18),
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  splashColor: Colors.white,
+                  icon: const Icon(Icons.tune),
+                  onPressed: () {
+                    /* TODO: Search by genre */
+                  },
+                ),
+              )),
+        ),
+      ],
     );
   }
 }
