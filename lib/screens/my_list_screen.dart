51313f5000d8c2947d1626757ae613bcfbb2cb0e
alexcorhe <alex.coronado@estudiantat.upc.edu>
2023-01-16 14:02:14 +0100

    prgreso en widget card de mylist

--- lib/screens/my_list_screen.dart
+++ lib/screens/my_list_screen.dart
@@ -10,26 +10,8 @@ class MyListScreen extends StatefulWidget {
 }
 
 class _MyListScreenState extends State<MyListScreen> {
-  Anime? anime;
-
-  @override
-  void initState() {
-    loadAnime(1).then((newAnime) {
-      setState(() {
-        anime = newAnime;
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (anime == null) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
     return Scaffold(
       body: Column(
         children: [
@@ -38,57 +20,102 @@ class _MyListScreenState extends State<MyListScreen> {
           ),
           Row(
             children: [
-              Column(
-                children: [
-                  //Image.network(anime!.image, height: 250),
-                  Image.network(
-                    fit: BoxFit.cover,
-                    'https://cdn.myanimelist.net/images/anime/1471/128323.jpg',
-                    height: 250,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 180,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
-
-                  Container(
-                    decoration: const BoxDecoration(color: Colors.black),
-                    child: Column(
-                      children: [
-                        Column(
-                          children: const [
-                            Text("NAME",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold))
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.black.withOpacity(0.2)),
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    "TV",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  Icon(Icons.star_border, color: Colors.white),
-                                  Text(
-                                    "SCORE",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  Icon(Icons.person, color: Colors.white),
-                                  Text(
-                                    "USERS",
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
-                              ),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      //imagen
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //Image.network(anime!.image, height: 250),
+                          Image.network(
+                            'https://cdn.myanimelist.net/images/anime/1471/128323.jpg',
+                            /* height: 150 */
+                          ),
+                        ],
+                      ),
+                      //info
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.6),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
                             ),
-                          ],
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  children: const [
+                                    Text("NAME",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold))
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      child: Row(
+                                        children: const [
+                                          Text(
+                                            "TV",
+                                            textAlign: TextAlign.left,
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          Icon(Icons.star_border,
+                                              color: Colors.white),
+                                          Text(
+                                            "SCORE",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          Icon(Icons.person,
+                                              color: Colors.white),
+                                          Text(
+                                            "USERS",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      //icon
+                      Align(
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.8),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(2)),
+                          ),
+                          child: const Icon(
+                            Icons.favorite_border,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ],
           ),
