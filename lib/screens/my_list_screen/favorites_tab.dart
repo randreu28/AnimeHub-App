640a8ecdd1cad81fe6d6b6d2510b4203fec53e9c
Rubén Chiquin <randreu28@gmail.com>
2023-01-22 11:44:53 +0100

    Añade la lista de animes favoritos

--- lib/screens/my_list_screen/favorites_tab.dart
+++ lib/screens/my_list_screen/favorites_tab.dart
@@ -1,19 +1,55 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:p1_coronado/utilities/loaders.dart';
+import 'package:p1_coronado/widgets/anime_card.dart';
 
-class FavoritesTab extends StatefulWidget {
+class FavoritesTab extends ConsumerWidget {
   const FavoritesTab({super.key});
 
   @override
-  State<FavoritesTab> createState() => _FavoritesTabState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteAnimes = ref.watch(loadFavoriteIDsProvider);
 
-class _FavoritesTabState extends State<FavoritesTab> {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Favorites tab"),
+    return favoriteAnimes.when(
+      data: (favoriteAnimes) {
+        if (favoriteAnimes.isEmpty) {
+          return const Center(
+            child: Text(
+              "You are not watching any anime yet",
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
+          );
+        }
+        return Scaffold(
+          body: GridView.count(
+            padding: const EdgeInsets.all(5),
+            crossAxisCount: 2,
+            childAspectRatio: 225 / 400, //Sizes of all images + text,
+            children: [
+              for (final id in favoriteAnimes) ...[AnimeCard(animeID: id)],
+            ],
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Text(
+            "An error has occured. Please try again later. ($error)",
+            maxLines: 2,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 15, color: Colors.red),
+          ),
+        );
+      },
+      loading: () {
+        return const Center(
+          child: SizedBox(
+              height: 200.0, child: LoadingIndicator(size: 20, borderWidth: 1)),
+        );
+      },
     );
   }
 }
