f38c5eab7aa8b24a98ea3dd9c18f3c4f605d73ca
Rubén Chiquin <randreu28@gmail.com>
2023-01-22 11:37:00 +0100

    Añade la lista de animes vistos

--- lib/screens/my_list_screen/watched_tab.dart
+++ lib/screens/my_list_screen/watched_tab.dart
@@ -1,19 +1,58 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:p1_coronado/models/anime.dart';
+import 'package:p1_coronado/utilities/loaders.dart';
+import 'package:p1_coronado/widgets/anime_card.dart';
 
-class WatchedTab extends StatefulWidget {
+class WatchedTab extends ConsumerWidget {
   const WatchedTab({super.key});
 
   @override
-  State<WatchedTab> createState() => _WatchedTabState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final watchedAnimes = ref.watch(loadStatusIDsProvider(
+      status: AnimeStatus.completed,
+    ));
 
-class _WatchedTabState extends State<WatchedTab> {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Watched tab"),
+    return watchedAnimes.when(
+      data: (watchedAnimes) {
+        if (watchedAnimes.isEmpty) {
+          return const Center(
+            child: Text(
+              "You are not watching any anime yet",
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
+          );
+        }
+        return Scaffold(
+          body: GridView.count(
+            padding: const EdgeInsets.all(5),
+            crossAxisCount: 2,
+            childAspectRatio: 225 / 400, //Sizes of all images + text,
+            children: [
+              for (final id in watchedAnimes) ...[AnimeCard(animeID: id)],
+            ],
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Text(
+            "An error has occured. Please try again later. ($error)",
+            maxLines: 2,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 15, color: Colors.red),
+          ),
+        );
+      },
+      loading: () {
+        return const Center(
+          child: SizedBox(
+              height: 200.0, child: LoadingIndicator(size: 20, borderWidth: 1)),
+        );
+      },
     );
   }
 }
