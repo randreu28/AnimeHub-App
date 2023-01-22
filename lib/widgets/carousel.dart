2cb303ddb06076cc0d18badd05d45b85aaf32269
Rubén Chiquin <randreu28@gmail.com>
2023-01-22 12:54:52 +0100

    Formataea el carousel para utilizar riverpod

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -1,101 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:p1_coronado/models/anime.dart';
 
-class Carousel extends StatelessWidget {
+class Carousel extends ConsumerWidget {
   final String title;
-  final List<Anime>? animes;
+  final AutoDisposeFutureProvider<List<Anime>> animes;
 
   const Carousel({super.key, required this.title, required this.animes});
 
   @override
-  Widget build(BuildContext context) {
-    if (animes == null) {
-      return Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(animes).when(
+      data: (animes) {
+        if (animes.isEmpty) {
+          return Column(
             children: [
-              Text(title, style: const TextStyle(fontSize: 16)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title, style: const TextStyle(fontSize: 16)),
+                ],
+              ),
+              const SizedBox(
+                height: 200.0,
+                width: 300,
+                child: Center(
+                  child: Text(
+                    "You haven't watched any anime! Watch some and we'll be able to recommend you new ones",
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ),
+              )
             ],
-          ),
-          const SizedBox(
-              height: 200.0, child: LoadingIndicator(size: 20, borderWidth: 1)),
-        ],
-      );
-    }
-
-    if (animes!.isEmpty) {
-      return Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(title, style: const TextStyle(fontSize: 16)),
-            ],
-          ),
-          const SizedBox(
-            height: 200.0,
-            width: 300,
-            child: Center(
-              child: Text(
-                "You haven't watched any anime! Watch some and we'll be able to recommend you new ones",
-                maxLines: 2,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 12, color: Colors.grey),
+          );
+        }
+        return Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: const TextStyle(fontSize: 16)),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              height: 250.0,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final anime = animes[index];
+                  return GestureDetector(
+                    onTap: () {
+                      /* TODO: Show specific anime screen */
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          anime.image!,
+                          height: 200,
+                        ),
+                        SizedBox(
+                          width: 130,
+                          child: Text(
+                            maxLines: 2,
+                            anime.title!,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: 20,
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(height: 1, indent: 5);
+                },
               ),
             ),
-          )
-        ],
-      );
-    }
-
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title, style: const TextStyle(fontSize: 16)),
           ],
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 20.0),
-          height: 250.0,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int index) {
-              final anime = animes![index];
-              return GestureDetector(
-                onTap: () {
-                  /* TODO: Show specific anime screen */
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      anime.image!,
-                      height: 200,
-                    ),
-                    SizedBox(
-                      width: 130,
-                      child: Text(
-                        maxLines: 2,
-                        anime.title!,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.left,
-                      ),
-                    )
-                  ],
-                ),
-              );
-            },
-            itemCount: 20,
-            separatorBuilder: (BuildContext context, int index) {
-              return const Divider(height: 1, indent: 5);
-            },
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "An error has occured. Please try again later. ($error)",
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 10, color: Colors.red),
+            ),
           ),
-        ),
-      ],
+        );
+      },
+      loading: () {
+        return Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: const TextStyle(fontSize: 16)),
+              ],
+            ),
+            const SizedBox(
+                height: 200.0,
+                child: LoadingIndicator(size: 20, borderWidth: 1)),
+          ],
+        );
+      },
     );
   }
 }
