64da5268542650deab96e3cb7fe12120c312ab88
Rubén Chiquin <randreu28@gmail.com>
2023-01-22 11:06:14 +0100

    Factoriza anime_card para utilizar un ID de parámetro
    Antes, se le porporcionaba el anime y printeaba los datos con él. Esto provocaba que la validación de datos, error y loading viviensen en el widget padre. Ahora, tan solo se le apasa un ID y el widget es autosuficiente, con estados loading, error y data

--- lib/widgets/anime_card.dart
+++ lib/widgets/anime_card.dart
@@ -1,106 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:p1_coronado/models/anime.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:p1_coronado/utilities/loaders.dart';
 
-class AnimeCard extends StatelessWidget {
-  final Anime anime;
-  const AnimeCard({Key? key, required this.anime}) : super(key: key);
+class AnimeCard extends ConsumerWidget {
+  final int animeID;
+  const AnimeCard({Key? key, required this.animeID}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        /* Go to tapped anime */
-      },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0),
-            ),
-            child: Stack(children: [
-              Image.network(
-                anime.image!,
-                fit: BoxFit.fill,
-                height: 270,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final animeProvider = ref.watch(loadAnimeProvider(
+      animeID: animeID,
+      hasDbData: false,
+    ));
+
+    return animeProvider.when(
+      data: (anime) {
+        return GestureDetector(
+          onTap: () {
+            /* Go to tapped anime */
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Stack(children: [
+                  Image.network(
+                    anime.image!,
+                    fit: BoxFit.fill,
+                    height: 270,
+                  ),
+                  SizedBox(
+                    height: 270,
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Card(
+                          color: Colors.black,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(anime.score!.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white)),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 15,
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(anime.members.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white)),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    const Icon(
+                                      Icons.people,
+                                      color: Colors.white,
+                                      size: 15,
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )),
+                    ),
+                  )
+                ]),
               ),
-              SizedBox(
-                height: 270,
-                child: Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Card(
-                      color: Colors.black,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(anime.score!.toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white)),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                const Icon(
-                                  Icons.star,
-                                  color: Colors.white,
-                                  size: 15,
-                                )
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(anime.members.toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white)),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                const Icon(
-                                  Icons.people,
-                                  color: Colors.white,
-                                  size: 15,
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      )),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: const [
+                    Text(
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      maxLines: 2,
+                      "Lorem ipsum siamet dolorum epsilon atra esterni onho teldhuim",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      maxLines: 1,
+                      "Lorem ipsum siamet dolorum epsilon atra esterni onho teldhuim",
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
                 ),
               )
-            ]),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              children: const [
-                Text(
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  maxLines: 2,
-                  "Lorem ipsum siamet dolorum epsilon atra esterni onho teldhuim",
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.left,
-                ),
-                SizedBox(height: 5),
-                Text(
-                  maxLines: 1,
-                  "Lorem ipsum siamet dolorum epsilon atra esterni onho teldhuim",
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.left,
-                ),
-              ],
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "An error has occured. Please try again later. ($error)",
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 10, color: Colors.red),
             ),
-          )
-        ],
-      ),
+          ),
+        );
+        ;
+      },
+      loading: () {
+        return const Center(
+          child: SizedBox(
+              height: 200.0, child: LoadingIndicator(size: 20, borderWidth: 1)),
+        );
+      },
     );
   }
 }
