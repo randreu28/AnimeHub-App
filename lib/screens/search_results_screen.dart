7b3539dd94017926ff92ef69253603e9d6683cf6
Rubén Chiquin <randreu28@gmail.com>
2023-01-27 12:48:36 +0100

    Crea una pantalla de resultados de búsqueda

---
+++ lib/screens/search_results_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:p1_coronado/utilities/loaders.dart';
+import 'package:p1_coronado/widgets/anime_card.dart';
+import 'package:p1_coronado/widgets/search_bar.dart';
+
+class SearchResultScreen extends ConsumerWidget {
+  final String query;
+  final List<int> genresID;
+  const SearchResultScreen(
+      {super.key, required this.query, required this.genresID});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final searchAnimeLoader = ref.watch(loadAnimeSearchProvider(
+      genresID: genresID,
+      query: query,
+    ));
+
+    return searchAnimeLoader.when(
+      data: (searchedAnimes) {
+        if (searchedAnimes.isEmpty) {
+          return Layout(
+            child: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text("No results for: "),
+                  Text(
+                    query,
+                    style: const TextStyle(color: Colors.red),
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+        return Layout(
+          child: GridView.count(
+            padding: const EdgeInsets.all(5),
+            crossAxisCount: 2,
+            childAspectRatio: 225 / 420,
+            children: [
+              for (final anime in searchedAnimes) ...[
+                if (anime != null) AnimeCard(anime: anime)
+              ]
+            ],
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Layout(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "An error has occured. Please try again later. ($error)",
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 10, color: Colors.red),
+              ),
+            ),
+          ),
+        );
+      },
+      loading: () {
+        return const Layout(
+          child: Center(
+            child: SizedBox(
+                height: 200.0,
+                child: LoadingIndicator(size: 20, borderWidth: 1)),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Layout extends StatelessWidget {
+  final Widget child;
+  const Layout({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Search results"),
+        ),
+        body: child);
+  }
+}
