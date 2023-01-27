01214fa1e455b8db3981824336ce7761b4d52803
Rubén Chiquin <randreu28@gmail.com>
2023-01-27 18:15:20 +0100

    Carga el usuario oddaelita

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,46 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:p1_coronado/utilities/loaders.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userLoad = ref.watch(loadUserProvider(username: "oddaelita"));
 
-class _ProfileScreenState extends State<ProfileScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(children: [
-        Column(
-          children: [
-            Row(children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Container(
-                  padding: const EdgeInsets.all(5.0),
-                  color: Colors.teal,
-                  child: Image.network(
-                    'https://i.pinimg.com/564x/88/d9/fa/88d9faa3959bedd1f0eca6fca1de7c67.jpg',
-                    height: 180,
+    return userLoad.when(
+      data: (user) {
+        return Scaffold(
+          body: ListView(children: [
+            Column(
+              children: [
+                Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      color: Colors.teal,
+                      child: Image.network(
+                        user.image!,
+                        height: 180,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Text(
-                    "oddaelita",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "oddaelita",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      )
+                    ],
                   )
-                ],
-              )
-            ]),
-          ],
-        )
-      ]),
+                ]),
+              ],
+            )
+          ]),
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "An error has occured. Please try again later. ($error)",
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 10, color: Colors.red),
+            ),
+          ),
+        );
+      },
+      loading: () {
+        return const Center(
+          child: SizedBox(
+              height: 200.0, child: LoadingIndicator(size: 20, borderWidth: 1)),
+        );
+      },
     );
   }
 }
